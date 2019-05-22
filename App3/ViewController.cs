ccca387a6334f531ba717066fe7e93d4e6f110ce
Venkatesh Krishnan <venkatesh.krishnan@saviantconsulting.com>
2019-05-22 15:41:33 +0530

    Add project files.

---
+++ App3/ViewController.cs
@@ -0,0 +1,26 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace App3
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad ()
+        {
+            base.ViewDidLoad ();
+            this.NavigationController.PushViewController(new UIViewController1(), true);
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning ()
+        {
+            base.DidReceiveMemoryWarning ();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
